a5a674eaecdbd7f474aa210a499aecdb6eb01970
raniv <rani.verla@gmail.com>
2026-07-13 17:20:04 -0700

    onboarded assessment service

---
+++ assessment/src/test/java/com/medilabo/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,15 @@
+package com.medilabo.assessment;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
